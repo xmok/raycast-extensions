3d9dc3443d74d1c8c2376a6b6ad4d433e0c809a0
xmok <30526133+xmok@users.noreply.github.com>
2025-11-11 08:19:45 +0500

    [Immich] view assets

--- extensions/immich/src/api-keys.tsx
+++ extensions/immich/src/api-keys.tsx
@@ -1,5 +1,5 @@
 import { getApiKeys } from "@immich/sdk";
-import { List } from "@raycast/api";
+import { Icon, List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
 import { initialize } from "./immich";
 
@@ -11,6 +11,6 @@ export default function APIKeys() {
     });
 
     return <List isLoading={isLoading}>
-        {data?.map(key => <List.Item key={key.id} title={key.name} />)}
+        {data?.map(key => <List.Item key={key.id} icon={Icon.Key} title={key.name} subtitle={key.permissions.join()} accessories={[{date: new Date(key.createdAt)}]} />)}
     </List>
 }
--- extensions/immich/src/immich.ts
+++ extensions/immich/src/immich.ts
@@ -2,5 +2,6 @@ import { init } from "@immich/sdk";
 import { getPreferenceValues } from "@raycast/api";
 
 const {base_url, api_key} = getPreferenceValues<Preferences>();
-
-export const initialize = () => init({ baseUrl: new URL("api", base_url).toString(), apiKey: api_key });
+const buildUrl = (route="") => new URL(route, base_url).toString();
+export const buildAuthenticatedUrl = (route:string) => `${buildUrl("api"+ route)}?apiKey=${api_key}`;
+export const initialize = () => init({ baseUrl: buildUrl("api"), apiKey: api_key });
