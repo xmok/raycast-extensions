47faa1b62c6873ec24653735463a23e170806ce2
xmok <30526133+xmok@users.noreply.github.com>
2025-11-15 06:36:22 +0500

    [Featurebase] ye olde lint + images

--- extensions/featurebase/src/manage-users.tsx
+++ extensions/featurebase/src/manage-users.tsx
@@ -4,16 +4,33 @@ import { Icon, List } from "@raycast/api";
 import { useState } from "react";
 
 export default function ManageUsers() {
-    const [searchText, setSearchText] = useState("");
-    const {isLoading, data: users, pagination}  = useCachedPromise((q: string) => async(options) => {
-        const result =  await featurebase.organization.identifyUser.query({page: options.page+1, q})
-            return {
-             data: result.results,
-hasMore: result.totalResults > result.page * result.limit            };
-    }
-    , [searchText], {initialData: []})
-    
-    return <List isLoading={isLoading} pagination={pagination} onSearchTextChange={setSearchText} throttle>
-{users.map(user=> <List.Item key={user.id} icon={{ source:user.profilePicture, fallback: Icon.Person}} title={user.name} subtitle={user.email} />)}
-</List>
+  const [searchText, setSearchText] = useState("");
+  const {
+    isLoading,
+    data: users,
+    pagination,
+  } = useCachedPromise(
+    (q: string) => async (options) => {
+      const result = await featurebase.organization.identifyUser.query({ page: options.page + 1, q });
+      return {
+        data: result.results,
+        hasMore: result.totalResults > result.page * result.limit,
+      };
+    },
+    [searchText],
+    { initialData: [] },
+  );
+
+  return (
+    <List isLoading={isLoading} pagination={pagination} onSearchTextChange={setSearchText} throttle>
+      {users.map((user) => (
+        <List.Item
+          key={user.id}
+          icon={{ source: user.profilePicture, fallback: Icon.Person }}
+          title={user.name}
+          subtitle={user.email}
+        />
+      ))}
+    </List>
+  );
 }
