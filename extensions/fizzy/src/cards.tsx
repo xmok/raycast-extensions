d5b1bc545072c18d214d0ba0c9dcafb441d69786
xmok <30526133+xmok@users.noreply.github.com>
2025-12-21 21:45:17 +0500

    [Fizzy] lint + images

--- extensions/fizzy/src/cards.tsx
+++ extensions/fizzy/src/cards.tsx
@@ -3,12 +3,25 @@ import { fizzy } from "./fizzy";
 import { Board } from "./types";
 import { Action, ActionPanel, List } from "@raycast/api";
 
-export default function Cards({board}: {board: Board}) {
-    const {isLoading, data: cards} = useCachedPromise((boardId: string) => fizzy.cards.list(boardId), [board.id], {initialData:[]})
+export default function Cards({ board }: { board: Board }) {
+  const { isLoading, data: cards } = useCachedPromise((boardId: string) => fizzy.cards.list(boardId), [board.id], {
+    initialData: [],
+  });
 
-    return <List isLoading={isLoading} isShowingDetail>
-        {cards.map(card => <List.Item key={card.id} title={card.title} detail={<List.Item.Detail markdown={card.description} />} actions={<ActionPanel>
-                    <Action.OpenInBrowser url={card.url} />
-        </ActionPanel>} />)}
+  return (
+    <List isLoading={isLoading} isShowingDetail>
+      {cards.map((card) => (
+        <List.Item
+          key={card.id}
+          title={card.title}
+          detail={<List.Item.Detail markdown={card.description} />}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser url={card.url} />
+            </ActionPanel>
+          }
+        />
+      ))}
     </List>
+  );
 }
